e4454e4e9aeedb6f092ae6575914a12182ec24bd
JacksonBey <jacksongrowson@gmail.com>
2023-06-25 00:25:26 -0700

    got it to upload into pinecone

--- scripts/santEpkToPinecone.py
+++ scripts/santEpkToPinecone.py
@@ -3,17 +3,28 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pinecone
+import spacy
+import os
+from dotenv import load_dotenv
+from pathlib import Path  
+import numpy as np
+
+env_path = Path('..') / '.env.local'
+load_dotenv(dotenv_path=env_path)
+
+nlp = spacy.load('en_core_web_lg')
 
 class UrlModel(BaseModel):
     url: str
 
 app = FastAPI()
 
-pinecone.init(api_key="ea5d0414-36b9-4d8c-92f0-4ecc903aceb8")
-index_name = "rapid-connect-poc"
+pinecone_key = os.getenv('PINECONE_API_KEY')
+pinecone_index_name = os.getenv('PINECONE_INDEX_NAME')
+pinecone_enviroment = os.getenv('PINECONE_ENVIROMENT')
 
-# pinecone.init(api_key=)
-# index_name = 
+pinecone.init(api_key=pinecone_key, environment=pinecone_enviroment)
+index_name = pinecone_index_name
 
 @app.post("/scrape_and_store")
 async def scrape_and_store_website(url_model: UrlModel):
@@ -21,22 +32,22 @@ async def scrape_and_store_website(url_model: UrlModel):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
-
-        image_urls = [image.get("src") for image in soup.find_all("img")]
         texts = [paragraph.text.strip() for paragraph in soup.find_all("p")]
-        hrefs = [link.get("href") for link in soup.find_all("a")]
-
-        # need to convert this to valid vector data
-        vector_data = image_urls + texts + hrefs
-
-
+        vector_data = texts
+        vector_embeddings = []
+        for i, text in enumerate(vector_data):
+            vector = np.array(nlp(text).vector.round(5).tolist())
+            # Check if vector is not a zero-vector
+            if np.count_nonzero(vector):
+                vector_embeddings.append((str(i), vector.tolist()))
+        
         pinecone_index = pinecone.Index(index_name)
-        pinecone_index.upsert(ids=vector_data, vectors=vector_data)
 
-        return {"message": "success"}
-
+        # Check if vectors are not empty and have correct dimension
+        if len(vector_embeddings) > 0 and len(vector_embeddings[0][1]) == nlp.vocab.vectors_length:
+            pinecone_index.upsert(vectors=vector_embeddings)
+            return {"message": "success"}
+        else:
+            return {"message": "failure", "error": "Generated invalid vectors."}
     except Exception as e:
         return {"message": "failure", "error": str(e)}
-
-
-
