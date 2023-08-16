8decf5353184f74ce970442ff905f813a59ca3ba
JacksonBey <jacksongrowson@gmail.com>
2023-08-16 14:38:52 -0700

    cleaned up SelectionRow

--- components/form/selectionRow.js
+++ components/form/selectionRow.js
@@ -2,12 +2,7 @@ import React, { useState } from 'react';
 import ImageCard from './imageCard';
 
 const SelectionRow = ({ images, onSelect }) => {
-  const [selectedImage, setSelectedImage] = useState("");
-
-  // const handleSelect = (title) => {
-  //   setSelectedImages([...selectedImages, title]);
-  // };
-  console.log('IMAGECARD? ', ImageCard)
+  const [selectedImage, setSelectedImage] = useState(images[0]?.title);
 
   const handleSelect = (e) => {
     console.log('e: ', e , e?.target, e?.target?.value, e?.value)
@@ -20,7 +15,6 @@ const SelectionRow = ({ images, onSelect }) => {
       {images.map((image, index) => (
         <ImageCard key={index} image={image} onSelect={handleSelect} selected={selectedImage === image.title}/>
       ))}
-      {/* <div>Selected Images: {selectedImages.join(', ')}</div> */}
     </div>
   );
 };
